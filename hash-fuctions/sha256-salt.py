f351be9970417ddcaf53abbfdefdc2a5bdac205c
virgilio21 <virgilio.padron.sop.j@gmail.com>
2022-12-13 13:49:23 -0600

    add random salt string

--- hash-fuctions/sha256-salt.py
+++ hash-fuctions/sha256-salt.py
@@ -1,18 +1,14 @@
-import uuid
 import hashlib
 
 
 def hash_text(text, salt=''):
 
-    print(salt)
     return hashlib.sha256(salt.encode() + text.encode()).hexdigest()
 
 
 if __name__ == '__main__':
 
-    salt = 'super_secret'
+    salt = '$2b$12$WaKsRhoClB7lGsrxN1j7fO'
     text = 'gatitos'
 
     sha256_text = hash_text(text, salt)
-    print(sha256_text)
-    print(len(sha256_text))
